47c8e09e4bfe0c79afc05a62f8f4caf094746b3a
Pedro <pedro.hbf3@gmail.com>
2022-08-20 17:03:02 +0200

    add license file

--- web/src/components/App.tsx
+++ web/src/components/App.tsx
@@ -3,7 +3,7 @@ import { Dashboard } from './Dasboard'
 
 export const App: React.FC = () => (
   <div className="bg-gray-900 text-white font-sans h-screen w-screen overflow-auto relative">
-    <nav className="bg-gray-800 p-4 mb-4 shadow-lg">
+    <nav className="bg-purple-800 p-4 mb-4 shadow-lg">
       test
     </nav>
     <main className="mx-4">
